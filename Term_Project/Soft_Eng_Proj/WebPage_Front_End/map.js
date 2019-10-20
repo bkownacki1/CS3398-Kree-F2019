a7bd9c5301197b48066b9e080539cddbbaca32b0
bkownacki1 <54732696+bkownacki1@users.noreply.github.com>
2019-10-20 14:38:14 -0500

    Fixed Shape of LBJ Polygon and added Pleasant Polygon
    Spent a lot of time trying to figure out the best way to do overlapping polygons for garages like the Matthew's Street Garage where multiple permits are allowed on different levels. I tried researching stripe fill patterns like are utilized on the Parking Services site, but they use a different overlay API which isn't available for Maps. Solution I came up with is to have an outline color for parking type with the fewest spaces and fill color for the parking type with the most spaces. Also considering adding another smaller polygon in the corner of the larger polygon but that would be a different polygon which would make filtering by color harder.
    
    Also found out that you don't need to add duplicate points for the start and end point of a polygon, it will just autoconnect the last drawn point with the first drawn one.
    
    Used OpenStreetMaps for the points where each lot/garage is located because they have a feature to "square" polygons and Google Maps API does not, so that allows us to have much more even shapes, even if they're a little bit off center from their building outline.
    
    Here is an example of the tool I used:  https://nominatim.openstreetmap.org/search.php?q=Pleasant+Street+Parking+Garage&polygon_geojson=1&viewbox=
    
    Clicking details and then clicking on the way under the OSM row in the details gives a set of nodes that the object is comprised of and clicking on those nodes gives the coordinates.
    
    It's a bit awkward to get these coordinates because they're formatted as links, but by holding alt before clicking them you can copy them without activating the hyperlink. That does download the webpage for me which is a bit weird, but deleting abunch of downloaded pages is a lot faster than fiddling with how you hover the mouse over a hyperlink to select it.
    
    I'll be adding more polygons later today.

--- Term_Project/Soft_Eng_Proj/WebPage_Front_End/map.js
+++ Term_Project/Soft_Eng_Proj/WebPage_Front_End/map.js
@@ -3,31 +3,66 @@ function initMap() {
       var San_Marcos = {lat: 29.8884, lng: -97.9384};
       var Lbj = {lat: 29.889280, lng: -97.944592};
       var Alkek = {lat: 29.888865, lng: -97.943075};
+      var greyFill = '#454545';
+      var redFill = '#ff0000';
+      var greenFill = '#0bd900';
+
       // The map, centered at San Marcos
       var map = new google.maps.Map(
           document.getElementById('google_map'), {zoom: 16, center: San_Marcos});
-      // The marker, positioned at San Marcos
+
+      // Markers, positioned in San Marcos
       var sanMarcosMarker = new google.maps.Marker({position: San_Marcos, map: map});
       var lbjMarker = new google.maps.Marker({position: Lbj, map: map, title: 'LBJ Student Center'});
       var alkMarker = new google.maps.Marker({position: Alkek, map: map, title: 'Alkek Library'});
 
+      //Polygons
+
+      //lBJ Garage
       var lbjGarageCoords = [
-          {lat:29.890024, lng: -97.945568},
-          {lat: 29.889455, lng: -97.944932},
-          {lat: 29.889233, lng: -97.945202},
-          {lat: 29.889818, lng: -97.945870},
-          {lat: 29.890024, lng: -97.945568}
+          {lat: 29.8900678, lng: -97.9456093},
+          {lat: 29.8894963, lng: -97.9449421},
+          {lat: 29.8892828, lng: -97.9451853},
+          {lat: 29.8893200, lng: -97.9452287},
+          {lat: 29.8892872, lng: -97.9452661},
+          {lat: 29.8898217, lng: -97.9458899},
         ];
 
         var lbjGarage = new google.maps.Polygon({
           paths: lbjGarageCoords,
-          strokeColor: '##b0b0b0',
+          strokeColor: greyFill,
           strokeOpacity: 0.8,
           strokeWeight: 2,
-          fillColor: '##b0b0b0',
+          fillColor: greyFill,
           fillOpacity: 0.35
         });
         lbjGarage.setMap(map);
+        //end LBJ Garage
+
+        //Pleasant Street Garage
+        var pleasantStreetGarageCoords = [
+            {lat: 29.8901117, lng: -97.9411670},
+            {lat: 29.8901606, lng: -97.9409888},
+            {lat: 29.8902194, lng: -97.9410085},
+            {lat: 29.8903540, lng: -97.9405135},
+            {lat: 29.8898840, lng: -97.9403461},
+            {lat: 29.8897133, lng: -97.9409828},
+            {lat: 29.8897619, lng: -97.9409969},
+            {lat: 29.8897386, lng: -97.9410669}
+          ];
+
+          var pleasantStreetGarage = new google.maps.Polygon({
+            paths: pleasantStreetGarageCoords,
+            strokeColor: redFill,
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: redFill,
+            fillOpacity: 0.35
+          });
+
+          pleasantStreetGarage.setMap(map);
+        //End Pleasant Street Garage
+
 }
 
 // Funtion to Update Map to New Location Based on User Input.
