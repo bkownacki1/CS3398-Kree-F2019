49c6bfaf6903ee9d2e7def2f5eb28f6df357117a
bkownacki1 <54732696+bkownacki1@users.noreply.github.com>
2019-10-29 14:33:15 -0500

    Attempted to add infowindow to lbjGarage
    Attempted to add infowindows to LBJ Garage. InfoWindows are meant for markers so it's a pain to add them to polygons. Still WIP.

--- Term_Project/Soft_Eng_Proj/WebPage_Front_End/map.js
+++ Term_Project/Soft_Eng_Proj/WebPage_Front_End/map.js
@@ -1,8 +1,12 @@
+var infowindow = new google.maps.InfoWindow({
+  size: new google.maps.Size(150,50)
+});
+
 function initMap() {
-    // The location of Uluru
+    //var geolib = google.maps.geometry.spherical;
     var San_Marcos = {lat: 29.8884, lng: -97.9384};
-    var Lbj = {lat: 29.889280, lng: -97.944592};
-    var Alkek = {lat: 29.888865, lng: -97.943075};
+    /*var Lbj = {lat: 29.889280, lng: -97.944592};
+    var Alkek = {lat: 29.888865, lng: -97.943075};*/
     var greyFill = '#454545';
     var redFill = '#ff0000';
     var greenFill = '#0bd900';
@@ -12,7 +16,16 @@ function initMap() {
 
     // The map, centered at San Marcos
     var map = new google.maps.Map(
-        document.getElementById('google_map'), {zoom: 15, center: San_Marcos, gestureHandling: 'greedy'});
+        document.getElementById('google_map'), {zoom: 15, center: San_Marcos, mapTypeControl:true,
+          mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
+          gestureHandling: 'greedy', navigationControl:true, mapTypeId: google.maps.MapTypeId.ROADMAP});
+
+    //event Listener declared
+    /*google.maps.event.addListener(map, 'click', function() {
+      infowindow.close();
+    });
+    bounds = new google.maps.LatLngBounds();*/
+
 
     // Markers, positioned in San Marcos. Commented out because they clutter the screen with all the lots
     //var sanMarcosMarker = new google.maps.Marker({position: San_Marcos, map: map});
@@ -35,15 +48,55 @@ function initMap() {
     ];
 
     var lbjGarage = new google.maps.Polygon({
-        paths: lbjGarageCoords,
+        map: map,
+        path: lbjGarageCoords,
         strokeColor: greyFill,
         strokeOpacity: 0.8,
         strokeWeight: 2,
         fillColor: greyFill,
-        fillOpacity: 0.5
+        fillOpacity: 0.5,
+        name: "lbjGarage"
     });
 
-    lbjGarage.setMap(map);
+    //lbjGarage.setMap(map);
+
+    google.maps.event.addListener(lbjGarage, 'click', function(event){
+      var contentString = "name" + this.name + "<br>" + "test..." + event.latLng.toUrlValue(6);
+        infowindow.setContent(contentString);
+        infowindow.setPosition(event.latLng);
+        infowindow.open(map);
+    });
+
+    /*for (var i=0;i<lbjGarage.getPath().getLength();i++){
+      bounds.extend(lbjGarage.getPath().getAt(i));
+    }*/
+/*'<div id="content">'+
+  '<div id="siteNotice">'+
+  '</div>'+
+  '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+  '<div id="bodyContent">'+
+  '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+  'sandstone rock formation in the southern part of the '+
+  'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+  'south west of the nearest large town, Alice Springs; 450&#160;km '+
+  '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+  'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+  'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+  'Aboriginal people of the area. It has many springs, waterholes, '+
+  'rock caves and ancient paintings. Uluru is listed as a World '+
+  'Heritage Site.</p>'+
+  '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+  'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+  '(last visited June 22, 2009).</p>'+
+  '</div>'+
+  '</div>'*/
+
+
+
+  /*lbjGarage.addListener('click'.function()){
+    infowindow.close(map, lbjGarage)
+  }*/
+
     //End LBJ Garage
 
     //Pleasant Street Garage
